e8d6a7888e64ed63852d4a505775e908b9a2125a
Stooneheart <lucie.favier@epfedu.fr>
2017-05-18 16:53:55 +0200

    LoginActivity

--- app/src/main/java/com/example/lucie/absences20/MainActivity.java
+++ app/src/main/java/com/example/lucie/absences20/MainActivity.java
@@ -1,20 +1,34 @@
 package com.example.lucie.absences20;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button;
+    EditText username;
+    EditText password;
+    String message;
+    TextView wrongPassword;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        username = (EditText) findViewById(R.id.editText3);
+        password = (EditText) findViewById(R.id.editText4);
+        Resources res = getResources();
+        message = String.format(res.getString(R.string.WPassword));
+        wrongPassword = (TextView) findViewById(R.id.textView2);
     }
+
+
 
     public void onClick(View v) {
         Intent intent = new Intent(this, AccueilActivity.class);
@@ -22,4 +36,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+
+
+    public void login(View view) {
+        if (username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
+            Intent intent = new Intent(this, AccueilActivity.class);
+            startActivity(intent);
+            //correcct password
+        } else {
+            //wrong password
+            wrongPassword.setText(message);
+        }
+
+    }
 }
