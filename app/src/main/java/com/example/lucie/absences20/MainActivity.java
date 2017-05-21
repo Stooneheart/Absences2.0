7a34b551a02a142330dab4c12aad28abf0264df8
Stooneheart <lucie.favier@epfedu.fr>
2017-05-21 16:40:36 +0200

    login qui ne marche putain de pas !

--- app/src/main/java/com/example/lucie/absences20/MainActivity.java
+++ app/src/main/java/com/example/lucie/absences20/MainActivity.java
@@ -2,12 +2,21 @@ package com.example.lucie.absences20;
 
 import android.content.Intent;
 import android.content.res.Resources;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,38 +24,80 @@ public class MainActivity extends AppCompatActivity {
     EditText password;
     String message;
     TextView wrongPassword;
+    Intent intent;
+    String type;
+    RequestQueue queue1;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        username = (EditText) findViewById(R.id.editText3);
-        password = (EditText) findViewById(R.id.editText4);
         Resources res = getResources();
         message = String.format(res.getString(R.string.WPassword));
         wrongPassword = (TextView) findViewById(R.id.textView2);
     }
 
 
-
     public void onClick(View v) {
-        Intent intent = new Intent(this, AccueilActivity.class);
-        startActivity(intent);
+        Intent intent1 = new Intent(this, AccueilActivity.class);
+        startActivity(intent1);
     }
 
 
-
-
     public void login(View view) {
-        if (username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
-            Intent intent = new Intent(this, AccueilActivity.class);
-            startActivity(intent);
-            //correcct password
-        } else {
-            //wrong password
-            wrongPassword.setText(message);
-        }
+        username = (EditText) findViewById(R.id.editText3);
+        password = (EditText) findViewById(R.id.editText4);
+        String usname = username.getText().toString();
+        String pword = password.getText().toString();
+        queue1 = Volley.newRequestQueue(this);
+        String url = "http://absence2epf.net16.net/api/token.php?identifiant="+ usname +"&pw=" +pword;
+        intent = new Intent(this, AccueilActivity.class);
 
+        StringRequest jsObjRequest = new StringRequest
+                (Request.Method.GET, url, new Response.Listener<String>() {
+
+                    @Override
+                    public void onResponse(String response) {
+                        String url1 = "http://absence2epf.net16.net/api/utilisateur.php?token=" + response;
+                        StringRequest jsObjReq = new StringRequest(Request.Method.GET, url1, new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+
+                                try {
+                                    JSONObject jsonType = new JSONObject(response);
+                                    type = jsonType.get("type").toString();
+
+                                    startActivity(intent);
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+
+                        }, new Response.ErrorListener() {
+
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                // TODO Auto-generated method stub
+                            }
+                        });
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO Auto-generated method stub
+                        wrongPassword.setText(message);
+
+                    }
+                });
+
+
+        queue1.add(jsObjRequest);
+    }
+
+    public String getType() {
+        return type;
     }
 }
